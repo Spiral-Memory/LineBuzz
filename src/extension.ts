c20ee598a136163a80abdac33d8f0a6cdadf3b38
Zishan Ahmad <zishan.barun@gmail.com>
2025-08-10 12:13:19 +0530

    handle login error
    Signed-off-by: Zishan Ahmad <zishan.barun@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -5,32 +5,48 @@ import { RocketChatProvider } from "./providers/rocketChatProvider";
 export async function activate(context: vscode.ExtensionContext) {
   const provider = new RocketChatProvider("http://localhost:3000");
 
-  await provider.login({
-    user: "zishan.barun@gmail.com",
-    password: "spiral_memory"
-  });
+  try {
+    await provider.login({
+      user: "zishan.barun@gmail.com",
+      password: "spiral_memory",
+    });
 
-  const buzzController = new BuzzCommentController(provider);
+    const buzzController = new BuzzCommentController(provider);
 
-  context.subscriptions.push(buzzController.commentController);
+    context.subscriptions.push(buzzController.commentController);
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand("linebuzz.startDiscussion", (initialMessage: vscode.CommentReply) => {
-      buzzController.startDiscussion(initialMessage);
-    })
-  );
+    context.subscriptions.push(
+      vscode.commands.registerCommand(
+        "linebuzz.startDiscussion",
+        (initialMessage: vscode.CommentReply) => {
+          buzzController.startDiscussion(initialMessage);
+        }
+      )
+    );
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand("linebuzz.reply", (reply: vscode.CommentReply) => {
-      buzzController.replyToMessage(reply);
-    })
-  );
+    context.subscriptions.push(
+      vscode.commands.registerCommand(
+        "linebuzz.reply",
+        (reply: vscode.CommentReply) => {
+          buzzController.replyToMessage(reply);
+        }
+      )
+    );
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand("linebuzz.refreshMsg", (thread: vscode.CommentThread) => {
-      buzzController.refreshMessage(thread);
-    })
-  );
+    context.subscriptions.push(
+      vscode.commands.registerCommand(
+        "linebuzz.refreshMsg",
+        (thread: vscode.CommentThread) => {
+          buzzController.refreshMessage(thread);
+        }
+      )
+    );
+  } catch (error) {
+    vscode.window.showErrorMessage(
+      "Login failed. Extension features will be disabled."
+    );
+    console.error("Login error:", error);
+  }
 }
 
 export function deactivate() {}
